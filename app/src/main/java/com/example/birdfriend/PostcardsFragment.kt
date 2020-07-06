44e4f7fae5c961ccbe21939965f4af3b890d11d4
vwhwang <vwhwang38@gmail.com>
2020-07-05 18:25:43 -0700

    initial set up

---
+++ app/src/main/java/com/example/birdfriend/PostcardsFragment.kt
@@ -0,0 +1,37 @@
+package com.example.birdfriend
+
+import android.graphics.drawable.AnimationDrawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class PostcardsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_postcards, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.outofpost_button).setOnClickListener {
+            findNavController().navigate(R.id.action_PostcardsFragment_to_SecondFragment)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/birdfriend/SecondFragment.kt
@@ -0,0 +1,48 @@
+package com.example.birdfriend
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
+import android.graphics.drawable.AnimationDrawable
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    lateinit var flyAnimation: AnimationDrawable
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.outside_button).setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+
+        view.findViewById<Button>(R.id.postcards_button).setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_PostcardsFragment)
+        }
+
+        val flyImage = view.findViewById<ImageView>(R.id.fly).apply {
+            setBackgroundResource(R.drawable.animation)
+            flyAnimation = background as AnimationDrawable
+        }
+//        flyAnimation.start()
+        flyImage.setOnClickListener({ flyAnimation.start() })
+
+
+    }
+}
